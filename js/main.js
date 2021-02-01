ea3e1e147da2ed6357aab175c3bdf4f7684045b0
dr-ak <alex-r-1982@mail.ru>
2021-02-01 12:58:17 +0700

    Нужно больше функций (Поправки)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // source link https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 const getRandomIntFromRange = (min, max) => {
-  if (min < 0 || max < 0) {
+  if (min < 0 || max < 0 || max <= min) {
     return null;
   }
   min = Math.ceil(min);
@@ -10,10 +10,10 @@ const getRandomIntFromRange = (min, max) => {
 };
 
 const getRandomFromRange = (min, max, decimalLength) => {
-  if (min < 0 || max < 0) {
+  if (min < 0 || max < 0 || max <= min) {
     return null;
   }
-  return + (Math.random() * (max - min) + min).toFixed(decimalLength);
+  return parseFloat((Math.random() * (max - min) + min).toFixed(decimalLength));
 };
 
 alert(getRandomIntFromRange(1, 3));
