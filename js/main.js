78eaf52bc0baab277a67f1c2467cd1de5143ea9c
dr-ak <alex-r-1982@mail.ru>
2021-02-05 11:56:26 +0700

    Больше деталей (Доработки)

--- js/main.js
+++ js/main.js
@@ -20,43 +20,34 @@ const getRandomFromRange = (min, max, decimalLength) => {
 // alert(getRandomFromRange(1.111, 3.666, 1));
 
 const OFFERS_COUNT = 10;
-const DEFAULT_AVATAR = 'img/avatars/default.png';
-const AVATAR = 'img/avatars/user{{xx}}.png';
-
-const avatars = ['01', '02', '03', '04', '05', '06', '07', '08', null, null];
-const getRandomAvatar = (avatars) => {
-  const userId = avatars.splice(getRandomIntFromRange(0, OFFERS_COUNT - 1), 1);
-  if (!userId[0]) {
-    return DEFAULT_AVATAR;
-  }
-  return AVATAR.replace('{{xx}}', userId);
-};
-const offerType = ['palace', 'flat', 'house', 'bungalow'];
+const offerTypes = ['palace', 'flat', 'house', 'bungalow'];
 const timePoints = ['12:00', '13:00', '14:00'];
 const offerFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-let photoId = 0;
-const photoUrl = 'http://o0.github.io/assets/images/tokyo/hotel{{x}}.jpg';
+const photos = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+const getRandomElement = arr => arr[getRandomIntFromRange(0, arr.length - 1)];
 
 const createOffer = () => {
   const locationX = getRandomFromRange(35.65000, 35.70000, 5);
   const locationY = getRandomFromRange(139.70000, 139.80000, 5);
   return {
-    author: {avatar: getRandomAvatar(avatars)},
+    author: {avatar: 'img/avatars/user0' + getRandomIntFromRange(1, 8) + '.png'},
     offer: {
       title: 'Лучншая недвижимость в мире',
       address: locationX + ', ' + locationY,
       price: getRandomIntFromRange(1000, 1000000),
-      type: offerType[getRandomIntFromRange(0, 3)],
+      type: getRandomElement(offerTypes),
       rooms: getRandomIntFromRange(1, 15),
       quests: getRandomIntFromRange(1, 30),
-      checkin: timePoints[getRandomIntFromRange(0, 2)],
-      checkout: timePoints[getRandomIntFromRange(0, 2)],
+      checkin: getRandomElement(timePoints),
+      checkout: getRandomElement(timePoints),
       features: offerFeatures.filter(() => getRandomIntFromRange(0, 1)),
       description: 'Только здесь и только сейчас, предложение ограничено!!!',
-      photos: new Array(getRandomIntFromRange(0, 15)).fill(null).map(() => {
-        photoId++;
-        return photoUrl.replace('{{x}}', photoId);
-      }),
+      photos: new Array(getRandomIntFromRange(0, 15)).fill(null).map(() => getRandomElement(photos)),
     },
     location: {
       x: locationX,
