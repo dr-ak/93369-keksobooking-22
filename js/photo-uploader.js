70c9ce48eea5224daa6b627062af4ffef78abf10
dr-ak <alex-r-1982@mail.ru>
2021-03-11 11:52:42 +0700

    Фото на память (Поправки к замечаниям)

--- js/photo-uploader.js
+++ js/photo-uploader.js
@@ -5,43 +5,44 @@ const avatarPreview = document.querySelector('.ad-form-header__preview').childre
 const imageFileChooser = document.querySelector('#images');
 const imagePreview = document.querySelector('.ad-form__photo');
 
-avatarFileChooser.addEventListener('change', () => {
-  const file = avatarFileChooser.files[0];
+const loadAvatarHandler = (evt) => {
+  avatarPreview.src = evt.target.result;
+};
+
+const loadImgHandler = (evt) => {
+  const img = document.createElement('img');
+  img.src = evt.target.result;
+  img.width = imagePreview.clientWidth;
+  img.height = imagePreview.clientHeight;
+  imagePreview.appendChild(img);
+};
+
+const uploadPhoto = (fileChooser, loadHandler) => {
+  const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
   const matches = FILE_TYPES.some((it) => {
     return fileName.endsWith(it);
   });
   if (matches) {
     const reader = new FileReader();
-    reader.addEventListener('load', () => {
-      avatarPreview.src = reader.result;
-    });
+    reader.addEventListener('load', loadHandler);
     reader.readAsDataURL(file);
   }
+};
+
+avatarFileChooser.addEventListener('change', () => {
+  uploadPhoto(avatarFileChooser, loadAvatarHandler);
 });
 
 imageFileChooser.addEventListener('change', () => {
-  const file = imageFileChooser.files[0];
-  const fileName = file.name.toLowerCase();
-  const matches = FILE_TYPES.some((it) => {
-    return fileName.endsWith(it);
-  });
-  if (matches) {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => {
-      const img = document.createElement('img');
-      img.src = reader.result;
-      img.width = imagePreview.clientWidth;
-      img.height = imagePreview.clientHeight;
-      imagePreview.appendChild(img);
-    });
-    reader.readAsDataURL(file);
-  }
+  uploadPhoto(imageFileChooser, loadImgHandler);
 });
 
 const cleanPhoto = () => {
   avatarPreview.src = 'img/muffin-grey.svg';
-  imagePreview.removeChild(imagePreview.children[0]);
+  for (let i = imagePreview.children.length - 1; i >= 0; i--) {
+    imagePreview.removeChild(imagePreview.children[0]);
+  }
 };
 
 export {cleanPhoto};
